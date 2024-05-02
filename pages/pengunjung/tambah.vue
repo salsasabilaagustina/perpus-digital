cc456799cbcb62637a78ce3c5c94484e740474d2
salsasabilaagustina <141603001+salsasabilaagustina@users.noreply.github.com>
2024-05-02 15:51:28 +0700

    Add files via upload

--- pages/pengunjung/tambah.vue
+++ pages/pengunjung/tambah.vue
@@ -1,52 +1,127 @@
 <template>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg 12">
-                <h2 class="text-center my-4">ISI BUKU KUNJUNGAN</h2>
-                <form>
-                    <div class="mb-3">
-                        <input type="text" class="form-control form control-lg rounded-5" placeholder="NAMA...">
-                    </div>
-                    <div class="mb-3">
-                        <select class="form-control form-conntrol-lg form-select rounded-5">
-                            <option value="">KATEGORI</option>
-                            <option value="Siswa">Siswa</option>
-                            <option value="Guru">Guru</option>
-                            <option value="Staf">Staf</option>
-                            <option value="Umum">Umum</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        
-                                <select class="form-control form-control-lg form-select rounded-5 mb-2">
-                                    <option value="">KELAS</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                </select>
-                    </div>
-                    <div class="mb-3">
-                        <select class="form-control form-control-lg form-select rounded-5">
-                            <option value="">KEPERLUAN</option>
-                            <option value="baca">Baca Buku</option>
-                            <option value="pinjam">Pinjam Buku</option>
-                            <option value="kembalikan">Kembalikan Buku</option>
-                        </select>
-                    </div>
-                    <button type="submit" class="btn btn-light btn-lg rounded-5 px-5">KIRIM</button>
-                </form>
+      <div class="row">
+        <div class="col-lg-12">
+          <h2 class="text-center my-4">ISI BUKU KUNJUNGAN</h2>
+          <form @submit.prevent="kirimData">
+            <div class="mb-3">
+              <input v-model="form.nama" type="text" class="form-control form-control-lg rounded-5" placeholder="Nama...">
             </div>
+            <div class="mb-3">
+              <select v-model="form.keanggotaan" class="keanggotaan form-control-lg form-select rounded-5">
+                <option value="">Keanggotaan</option>
+                <option v-for="(member, i) in members" :key="i" :value="member.id">{{ member.nama }}</option>
+              </select>
+            </div>
+            <div v-if="form.keanggotaan == 1" class="mb-3">
+              <div class="row">
+                <div class="col-md-4">
+                  <select v-model="form.tingkat" class="tingkat form-control-lg form-select rounded-5 mb-2">
+                    <option value="">kelas</option>
+                    <option value="1">X</option>
+                    <option value="2">XI</option>
+                    <option value="3">XII</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <select v-model="form.jurusan" class="jurusan form-control-lg form-select rounded-5 mb-2">
+                    <option value="">Jurusan</option>
+                    <option value="PPLG">PPLG</option>
+                    <option value="TJKT">TJKT</option>
+                    <option value="TSM">TSM</option>
+                    <option value="TOI">TOI</option>
+                    <option value="DKV">DKV</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <select v-model="form.kelas" class="form-control form-control-lg form-select rounded-5 mb-2">
+                    <option value="">tingkat</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="mb-3">
+              <select v-model="form.keperluan" class="form-control form-control-lg form-select rounded-5">
+                <option value="">Keperluan</option>
+                <option v-for="(item,i) in objectives" :key="i" :value="item.id">{{ item.nama }}</option>
+              </select>
+            </div>
+            <Nuxt-Link to="/pengunjung">
+              <button type="submit" class="btn btn-light btn-lg rounded-5 px-5">KIRIM</button>
+            </Nuxt-Link>
+          </form>
         </div>
+      </div>
     </div>
-</template>
+  </template>
+
+<script setup>
+const supabase = useSupabaseClient()
+
+const members = ref([])
+const objectives = ref([])
+const form =ref({
+    nama:"",
+    keanggotaan:"",
+    tingkat:"",
+    jurusan:"",
+    kelas:"",
+    keperluan:"",
+})
+const kirimData = async() => {
+    const { error } = await supabase.from('pengunjung').insert([form.value])
+    if(!error)navigateTo('/pengunjung')
+}
+
+const getkeanggotaan = async () => {
+    const { data,error } = await supabase.from('keanggotaan').select('*')
+    if(data) members.value = data
+}
+
+const getkeperluan = async () => {
+    const { data, error } = await supabase.from('keperluan').select('*')
+    if(data) objectives.value = data
+}
+
+onMounted(() => {
+    getkeanggotaan()
+    getkeperluan()
+})
+</script>
+
 
 <style scoped>
 .form-control {
     background-color: #D9D9D9;
 }
 
-.btn{
+.form-keanggotaan {
     background-color: #D9D9D9;
 }
-</style>
+
+.btn {
+    background-color: #D9D9D9;
+}
+.form-kelas{
+    background-color: #D9D9D9;
+}
+.form-keperluan{
+    background-color: #D9D9D9;
+}
+.form-jurusan{
+    background-color: #D9D9D9;
+}
+.tingkat{
+    background-color: #D9D9D9;
+}
+.jurusan {
+    background-color: #D9D9D9;
+}
+.keanggotaan {
+    background-color: #D9D9D9;
+}
+</style> 
